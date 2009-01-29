d4750ff43f9aaeb3edf7554557501874681c3afd
Lorenzo <zeelot3k@gmail.com>
2009-01-29 01:07:59 -0500

    started menu manager branch

--- themes/views/default/html/menu/main.php
+++ themes/views/default/html/menu/main.php
@@ -2,7 +2,6 @@
 	<h3>Main Menu</h3>
 	<ul>
 		<li><?php echo html::anchor('main/home', 'Home'); ?></li>
-		<li><?php echo html::anchor('main/home', 'Test_ACL_1'); ?></li>
-		<li><?php echo html::anchor('main/home', 'TEST_ACL_2'); ?></li>
+		<li><?php echo html::anchor('main/home', 'Admin'); ?></li>
 	</ul>
 </div>
