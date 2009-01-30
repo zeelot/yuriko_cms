9573ca9396d484f9182a8bd03db2734940c6c328
Lorenzo <zeelot3k@gmail.com>
2009-01-30 00:32:45 -0500

    no need to check the acl in the controller...this is done automatically in simple_acl module now

--- modules/user/controllers/user.php
+++ modules/user/controllers/user.php
@@ -15,36 +15,29 @@ class User_Controller extends Website_Controller{
 	{
 		widget::set('user_info', Component::factory('widget_user')->method('info'));
 		$this->profiler = new Profiler;
-		if(Auth::instance()->logged_in())
+		$user = Session::instance()->get('auth_user');
+		if(isset($_POST['profile_form']))
 		{
-			$user = Session::instance()->get('auth_user');
-			if(isset($_POST['profile_form']))
+			$profile = $user->profile;
+			if($profile->validate($_POST))
 			{
-				$profile = $user->profile;
-				if($profile->validate($_POST))
-				{
-					$profile->load_values($_POST->as_array());
-					$profile->save();
-				}
-				if(isset($_POST['password']) AND ($_POST['password'] != ''))
-				{
-					//if($user->change_password($_POST->as_array(), TRUE))
-					//{
-					//	url::redirect('user/profile');
-					//}
-				}
-				else
-				{
-					url::redirect('user/profile');
-				}
+				$profile->load_values($_POST->as_array());
+				$profile->save();
 			}
-			$this->template->content = View::factory('content/user/profile')
+			if(isset($_POST['password']) AND ($_POST['password'] != ''))
+			{
+				//if($user->change_password($_POST->as_array(), TRUE))
+				//{
+				//	url::redirect('user/profile');
+				//}
+			}
+			else
+			{
+				url::redirect('user/profile');
+			}
+		}
+		$this->template->content = View::factory('content/user/profile')
 				->set('user', $user);
-		}
-		else
-		{
-			$this->template->content = View::factory('errors/login_required');
-		}
 		
 	}
 	public function memberlist()
