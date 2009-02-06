1359acf6e934b3a0edd3aebb7aa4e0436cef7971
Lorenzo <zeelot3k@gmail.com>
2009-02-06 18:54:56 -0500

    initial structure

---
+++ modules/orm_content/models/page.php
@@ -0,0 +1,7 @@
+<?php defined('SYSPATH') OR die('No direct access allowed.');
+
+class Page_Model extends ORM {
+
+	private $has_many = array('sections');
+
+} // End Page Model
---
+++ modules/orm_content/models/partial.php
@@ -0,0 +1,7 @@
+<?php defined('SYSPATH') OR die('No direct access allowed.');
+
+class Partial_Model extends ORM {
+
+	// This class can be replaced or extended
+
+} // End Partial Model
