ed200212c6d461de6effd1c45e408abbe195d48e
Lorenzo <zeelot3k@gmail.com>
2009-03-07 16:51:59 -0500

    extended arr to include a recursive in_array method

---
+++ application/helpers/MY_arr.php
@@ -0,0 +1,18 @@
+<?php
+class arr extends arr_Core
+{
+	/*
+	 * Recursive version of in_array()
+	 */
+    public static function in_array($needle, $haystack)
+    {
+        foreach ($haystack as $stack)
+        {
+            if ($needle === $stack || (is_array($stack) && self::in_array($needle, $stack)))
+            {
+                return TRUE;
+            }
+        }
+        return FALSE;
+    }
+}
