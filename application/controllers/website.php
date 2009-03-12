3d0041b19bec9195374ec4b4de7403dc54e27455
Lorenzo <zeelot3k@gmail.com>
2009-03-12 13:10:45 -0400

    too much redundant code after all

--- application/controllers/website.php
+++ application/controllers/website.php
@@ -1,22 +1,12 @@
 <?php
-/*
- * No longer extends Template_Controller because I needed to edit a few things
- * $template = FALSE; now does not throw an error but you will
- * have to specify a template view before assigning vars.
- * mainly for db content
- * 
- */
-abstract class Website_Controller extends Controller{
+abstract class Website_Controller extends Template_Controller{
 
 	// Template view name
 	public $template = 'template/default';
 
-	// Default to do auto-rendering
-	public $auto_render = TRUE;
-
 	public function __construct()
 	{
-		parent::__construct();
+		Controller::__construct();//no longer want template::construct
 
 		// Load the template (if it's set)
 		if($this->template)
@@ -35,17 +25,5 @@ abstract class Website_Controller extends Controller{
 		//login widget for pages to use
 		widget::set('user_info', Component::factory('widget_user')->method('info'));
 		$this->profiler = new Profiler;
-	}
-
-	/**
-	 * Render the loaded template.
-	 */
-	public function _render()
-	{
-		if ($this->auto_render == TRUE)
-		{
-			// Render the template when the class is destroyed
-			$this->template->render(TRUE);
-		}
 	}
 } // End Website_Controller
