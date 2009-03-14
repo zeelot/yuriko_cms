192f16d1e85baecc1bc31f2a589cc84888566cd1
Lorenzo <zeelot3k@gmail.com>
2009-03-13 20:29:36 -0400

    bit of formatting cleanup and it now validates for xhtml

--- themes/default/views/content/navigation/default.php
+++ themes/default/views/content/navigation/default.php
@@ -2,16 +2,28 @@
 	<h2><?php echo $node->name; ?></h2>
 	<?php $level = $node->level; ?>
 	<?php foreach($node->subtree()->find_all() as $child): ?>
-		<?php if($child->level > $level): ?>
-		<ul><li><?php echo ($child->anchor)? html::anchor($child->anchor, $child->name) : $child->name; ?>
-		<?php $level++; ?>
-		<?php elseif($child->level < $level): ?>
-		<?php echo str_repeat('</li></ul>', $child->level < $level); ?>
-		<li><?php echo ($child->anchor)? html::anchor($child->anchor, $child->name) : $child->name; ?>
+		<?php if($child->level > $level): ?> 
+	<div><ul>
+		<li>
+		<?php echo ($child->anchor)
+						? html::anchor($child->anchor, $child->name)
+						: $child->name; ?>
+		<?php $level++; ?> 
+		<?php elseif($child->level < $level): ?> 
+		<?php echo str_repeat('</li></ul></div>', $level - $child->level); ?> 
+		</li>
+		<li>
+		<?php echo ($child->anchor)?
+						html::anchor($child->anchor, $child->name)
+						: $child->name; ?>
 		<?php $level = $child->level; ?>
-		<?php else: ?>
-		</li><li><?php echo ($child->anchor)? html::anchor($child->anchor, $child->name) : $child->name; ?>
-		<?php endif; ?>
-	<?php endforeach; ?>
-	</ul>
+		<?php else: ?> 
+		</li>
+		<li>
+		<?php echo ($child->anchor)
+						? html::anchor($child->anchor, $child->name)
+						: $child->name; ?>
+		<?php endif; ?> 
+	<?php endforeach; ?> 
+	<?php echo str_repeat('</li></ul></div>', $level - $node->level); ?> 
 </div>
