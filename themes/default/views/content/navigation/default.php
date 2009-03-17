142be8bead1641142dd967b06026455f6e8f3b37
Lorenzo <zeelot3k@gmail.com>
2009-03-16 21:28:05 -0400

    cleaned navigation view for some DRY

--- themes/default/views/content/navigation/default.php
+++ themes/default/views/content/navigation/default.php
@@ -5,25 +5,19 @@
 		<?php if($child->level > $level): ?> 
 	<div><ul>
 		<li>
-		<?php echo ($child->anchor)
-						? html::anchor($child->anchor, $child->name)
-						: $child->name; ?>
 		<?php $level++; ?> 
 		<?php elseif($child->level < $level): ?> 
 		<?php echo str_repeat('</li></ul></div>', $level - $child->level); ?> 
 		</li>
 		<li>
-		<?php echo ($child->anchor)?
-						html::anchor($child->anchor, $child->name)
-						: $child->name; ?>
 		<?php $level = $child->level; ?>
 		<?php else: ?> 
 		</li>
 		<li>
+		<?php endif; ?>
 		<?php echo ($child->anchor)
 						? html::anchor($child->anchor, $child->name)
 						: $child->name; ?>
-		<?php endif; ?> 
 	<?php endforeach; ?> 
 	<?php echo str_repeat('</li></ul></div>', $level - $node->level); ?> 
 </div>
