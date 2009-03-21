3651557503e9dcac92c4d701faefe916bdb60a04
Lorenzo <zeelot3k@gmail.com>
2009-03-21 16:01:52 -0400

    removed gmaps module...dont see myself using it anytime soon

--- modules/gmaps/i18n/en_US/gmaps.php
+++
@@ -1,7 +0,0 @@
-<?php defined('SYSPATH') OR die('No direct access allowed.');
-
-$lang = array
-(
-	'invalid_marker'		=>	'Invalid marker params (latitude: %s, longitude: %s)',
-	'invalid_dimensions'	=>	'Invalid map dimensions (width: %s, height: %s)',
-);
--- modules/gmaps/i18n/ru_RU/gmaps.php
+++
@@ -1,7 +0,0 @@
-<?php defined('SYSPATH') OR die('No direct access allowed.');
-
-$lang = array
-(
-	'invalid_marker'	=>	'Некорректные координаты маркера (широта: %s, долгота: %s)',
-	'invalid_dimensions'	=>	'Некорректные размеры карты (ширина: %s, высота: %s)',
-);
