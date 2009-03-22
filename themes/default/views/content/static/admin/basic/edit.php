f1122e54bf3de471db9a37bf4d051de2259bc5d0
Lorenzo <zeelot3k@gmail.com>
2009-03-22 10:16:56 -0400

    quick documentation! yay

--- themes/default/views/content/static/admin/basic/edit.php
+++ themes/default/views/content/static/admin/basic/edit.php
@@ -4,7 +4,16 @@
 	<fieldset>
 		<legend>Edit Basic Content</legend>
 		<input type="hidden" name="edit_basic_content" value="TRUE" />
+		<p class="info">
+		The page Name is the unique Identifier.  It can contain most character
+		and letters but must not contain spaces. Ex: contact_us.
+		</p>
 		<label>Name: <input name="name" type="text" value="<?php echo $item->name; ?>" /></label>
+		<p class="info">
+		The GUI for the content only works with Markdown at the moment.
+		However, you can select HTML as the format and type up your content
+		in HTML manually.
+		</p>
 		<label>Format:
 			<select name="format_id">
 				<?php foreach ($formats as $id => $name ): ?>
