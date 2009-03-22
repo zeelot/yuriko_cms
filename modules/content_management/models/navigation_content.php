9c5b7d2a4142c8852ad3630b24cbbf38c03e5df6
Lorenzo <zeelot3k@gmail.com>
2009-03-21 20:21:15 -0400

    little bugfix... @TODO: look into valid chars for urls

--- modules/content_management/models/navigation_content.php
+++ modules/content_management/models/navigation_content.php
@@ -21,7 +21,7 @@ class Navigation_Content_Model extends ORM_MPTT implements Content_Model{
 			->add_rules('tag', 'required', 'length[4,52]', array($this, 'unique_tag'))
 			->add_rules('parent_id', 'required', 'digit', array($this, 'valid_node'))
 			->add_rules('page_id', 'digit')
-			->add_rules('anchor', 'chars[a-zA-Z0-9_./]');
+			->add_rules('anchor', 'chars[a-zA-Z0-9_./:]');
 
 		return parent::validate($array, $save);
 	}
@@ -32,7 +32,7 @@ class Navigation_Content_Model extends ORM_MPTT implements Content_Model{
 			->add_rules('name', 'required', 'length[4,52]', 'chars[a-z A-Z0-9_.]')
 			->add_rules('tag', 'required', 'length[4,52]')
 			->add_rules('page_id', 'digit')
-			->add_rules('anchor', 'chars[a-zA-Z0-9_./]');
+			->add_rules('anchor', 'chars[a-zA-Z0-9_./:]');
 
 		return parent::validate($array, TRUE);
 	}	
