37567d4a7487559d6c19eb0c30949ec3341590a9
Lorenzo <zeelot@zeelot-mint.(none)>
2009-03-31 20:19:18 -0400

    missed a little thickbox bug =P

--- themes/default/views/admin/plugins/manage.php
+++ themes/default/views/admin/plugins/manage.php
@@ -16,11 +16,12 @@
 					array('alt' => 'Info', 'title' => 'Info')),
 				array('class' => 'thickbox')); ?>
 			<?php if(!$plugin->enabled): ?>
-			<?php echo html::anchor('admin/plugins/enable/'.$plugin->id,
+			<?php echo html::anchor('admin/plugins/enable/'.$plugin->id.'?height=200',
 				html::image('media/images/fam_silk/add.png',
-					array('alt' => 'Enable', 'title' => 'Add'))); ?>
+					array('alt' => 'Enable', 'title' => 'Add')),
+				array('class' => 'thickbox')); ?>
 			<?php else: ?>
-			<?php echo html::anchor('admin/plugins/disable/'.$plugin->id.'?height=300',
+			<?php echo html::anchor('admin/plugins/disable/'.$plugin->id.'?height=200',
 				html::image('media/images/fam_silk/delete.png',
 					array('alt' => 'Remove', 'title' => 'Remove')),
 				array('class' => 'thickbox')); ?>
