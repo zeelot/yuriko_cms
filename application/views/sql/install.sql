b6cf10d4783f9c5bee5609261bb4f4ce7c3cfcba
unknown <Zeelot@.(none)>
2009-04-29 12:21:53 -0400

    updated sql install file

--- application/views/sql/install.sql
+++ application/views/sql/install.sql
@@ -2,20 +2,20 @@ SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 
 DROP TABLE IF EXISTS `basic_contents`;
 CREATE TABLE IF NOT EXISTS `basic_contents` (
-  `id` tinyint(11) NOT NULL auto_increment,
-  `node_id` int(11) NOT NULL,
-  `format_id` tinyint(11) NOT NULL default '1',
-  `view` varchar(127) collate utf8_unicode_ci NOT NULL default 'content/basic/default',
+  `id` int(11) unsigned NOT NULL auto_increment,
+  `node_id` int(11) unsigned NOT NULL,
+  `format_id` int(11) unsigned NOT NULL default '1',
+  `view` varchar(127) collate utf8_unicode_ci NOT NULL default 'default',
   `name` varchar(125) collate utf8_unicode_ci NOT NULL,
   `content` longtext collate utf8_unicode_ci NOT NULL,
   `html` longtext collate utf8_unicode_ci NOT NULL,
   PRIMARY KEY  (`id`),
   UNIQUE KEY `name` (`name`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=16 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=21 ;
 
 DROP TABLE IF EXISTS `content_formats`;
 CREATE TABLE IF NOT EXISTS `content_formats` (
-  `id` tinyint(11) NOT NULL auto_increment,
+  `id` int(11) unsigned NOT NULL auto_increment,
   `name` varchar(255) collate utf8_unicode_ci NOT NULL,
   PRIMARY KEY  (`id`),
   UNIQUE KEY `name` (`name`)
@@ -27,40 +27,52 @@ INSERT INTO `content_formats` (`id`, `name`) VALUES
 
 DROP TABLE IF EXISTS `content_nodes`;
 CREATE TABLE IF NOT EXISTS `content_nodes` (
-  `id` int(11) NOT NULL auto_increment,
-  `content_type_id` int(11) NOT NULL,
-  `content_id` int(11) NOT NULL,
+  `id` int(11) unsigned NOT NULL auto_increment,
+  `content_type_id` int(11) unsigned NOT NULL,
+  `content_id` int(11) unsigned NOT NULL,
   `name` varchar(127) collate utf8_unicode_ci NOT NULL,
   `alias` varchar(127) collate utf8_unicode_ci NOT NULL,
-  `template` varchar(127) collate utf8_unicode_ci NOT NULL default 'default',
   PRIMARY KEY  (`id`),
   UNIQUE KEY `alias` (`alias`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=51 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=62 ;
 
 DROP TABLE IF EXISTS `content_pages`;
 CREATE TABLE IF NOT EXISTS `content_pages` (
-  `id` int(11) NOT NULL auto_increment,
+  `id` int(11) unsigned NOT NULL auto_increment,
   `alias` varchar(127) collate utf8_unicode_ci NOT NULL,
   `name` varchar(127) collate utf8_unicode_ci NOT NULL,
   `template` varchar(127) collate utf8_unicode_ci NOT NULL default 'default',
+  `locked` tinyint(1) NOT NULL default '0',
   PRIMARY KEY  (`id`),
   UNIQUE KEY `alias` (`alias`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=13 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=20 ;
+
+DROP TABLE IF EXISTS `content_page_inheritances`;
+CREATE TABLE IF NOT EXISTS `content_page_inheritances` (
+  `id` int(11) unsigned NOT NULL auto_increment,
+  `content_page_id` int(11) unsigned NOT NULL,
+  `inherited_page_id` int(11) unsigned NOT NULL,
+  PRIMARY KEY  (`content_page_id`,`inherited_page_id`,`id`),
+  KEY `pages_has_inheritances_FKIndex2` (`inherited_page_id`),
+  KEY `page_inheritances_FKIndex2` (`content_page_id`),
+  KEY `id` (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=2 ;
 
 DROP TABLE IF EXISTS `content_pivots`;
 CREATE TABLE IF NOT EXISTS `content_pivots` (
-  `id` int(11) NOT NULL auto_increment,
-  `content_page_id` int(11) NOT NULL,
-  `content_node_id` int(11) NOT NULL,
+  `id` int(11) unsigned NOT NULL auto_increment,
+  `content_page_id` int(11) unsigned NOT NULL,
+  `content_node_id` int(11) unsigned NOT NULL,
   `section` int(11) NOT NULL,
+  `view` varchar(127) collate utf8_unicode_ci NOT NULL default 'default',
   PRIMARY KEY  (`id`),
   KEY `has_nodes_FKIndex2` (`content_node_id`),
   KEY `has_pages_FKIndex2` (`content_page_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=17 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=4 ;
 
 DROP TABLE IF EXISTS `content_types`;
 CREATE TABLE IF NOT EXISTS `content_types` (
-  `id` int(11) NOT NULL auto_increment,
+  `id` int(11) unsigned NOT NULL auto_increment,
   `name` varchar(50) collate utf8_unicode_ci NOT NULL,
   PRIMARY KEY  (`id`),
   UNIQUE KEY `name` (`name`)
@@ -72,9 +84,9 @@ INSERT INTO `content_types` (`id`, `name`) VALUES
 
 DROP TABLE IF EXISTS `navigation_contents`;
 CREATE TABLE IF NOT EXISTS `navigation_contents` (
-  `id` tinyint(10) NOT NULL auto_increment,
-  `node_id` int(11) NOT NULL,
-  `page_id` int(11) NOT NULL default '0' COMMENT 'if this is set, the item becomes a link to the page using its alias',
+  `id` int(10) unsigned NOT NULL auto_increment,
+  `node_id` int(11) unsigned NOT NULL,
+  `page_id` int(11) unsigned NOT NULL default '0' COMMENT 'if this is set, the item becomes a link to the page using its alias',
   `level` int(11) NOT NULL,
   `lft` tinyint(10) NOT NULL,
   `rgt` tinyint(10) NOT NULL,
@@ -84,22 +96,22 @@ CREATE TABLE IF NOT EXISTS `navigation_contents` (
   `anchor` varchar(255) collate utf8_unicode_ci default NULL,
   PRIMARY KEY  (`id`),
   UNIQUE KEY `tag` (`tag`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=44 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=50 ;
 
 DROP TABLE IF EXISTS `plugins`;
 CREATE TABLE IF NOT EXISTS `plugins` (
   `id` int(11) NOT NULL auto_increment,
-  `name` varchar(127) NOT NULL,
-  `dir` varchar(127) NOT NULL,
-  `description` text NOT NULL,
-  `dependencies` text,
-  `notice_enable` text,
-  `notice_disable` text,
+  `name` varchar(127) collate utf8_unicode_ci NOT NULL,
+  `dir` varchar(127) collate utf8_unicode_ci NOT NULL,
+  `description` text collate utf8_unicode_ci NOT NULL,
+  `dependencies` text collate utf8_unicode_ci,
+  `notice_enable` text collate utf8_unicode_ci,
+  `notice_disable` text collate utf8_unicode_ci,
   `enabled` binary(1) NOT NULL default '0',
-  `version` varchar(10) NOT NULL,
+  `version` varchar(10) collate utf8_unicode_ci NOT NULL,
   PRIMARY KEY  (`id`),
   UNIQUE KEY `dir` (`dir`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=4 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=6 ;
 
 DROP TABLE IF EXISTS `roles`;
 CREATE TABLE IF NOT EXISTS `roles` (
@@ -123,7 +135,6 @@ CREATE TABLE IF NOT EXISTS `roles_users` (
   KEY `roles_users_FKIndex2` (`user_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
-
 INSERT INTO `roles_users` (`role_id`, `user_id`) VALUES
 (1, 60),
 (1, 63),
@@ -138,19 +149,33 @@ CREATE TABLE IF NOT EXISTS `sessions` (
   PRIMARY KEY  (`session_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
-
 DROP TABLE IF EXISTS `site_settings`;
 CREATE TABLE IF NOT EXISTS `site_settings` (
   `id` int(11) NOT NULL auto_increment,
-  `key` varchar(50) character set utf8 collate utf8_unicode_ci NOT NULL,
-  `value` varchar(255) character set utf8 collate utf8_unicode_ci NOT NULL,
+  `key` varchar(50) collate utf8_unicode_ci NOT NULL,
+  `value` varchar(255) collate utf8_unicode_ci NOT NULL,
   PRIMARY KEY  (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=5 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=4 ;
 
 INSERT INTO `site_settings` (`id`, `key`, `value`) VALUES
 (1, 'site_name', 'Zeelots Site'),
 (2, 'site_description', 'Just a sample site'),
 (3, 'site_theme', 'yuriko_cms');
+
+DROP TABLE IF EXISTS `themes`;
+CREATE TABLE IF NOT EXISTS `themes` (
+  `id` int(11) unsigned NOT NULL auto_increment,
+  `name` varchar(50) character set utf8 collate utf8_unicode_ci default NULL,
+  `dir` varchar(50) character set utf8 collate utf8_unicode_ci default NULL,
+  PRIMARY KEY  (`id`),
+  UNIQUE KEY `uniq_name` (`name`),
+  UNIQUE KEY `uniq_dir` (`dir`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=4 ;
+
+INSERT INTO `themes` (`id`, `name`, `dir`) VALUES
+(1, 'Default Theme', 'default'),
+(2, 'Zeelot''s Sandbox Theme', 'zeelots_sandbox'),
+(3, 'Zeelot''s Other Theme', 'zeelots_other_theme');
 
 DROP TABLE IF EXISTS `users`;
 CREATE TABLE IF NOT EXISTS `users` (
@@ -166,8 +191,28 @@ CREATE TABLE IF NOT EXISTS `users` (
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=64 ;
 
 INSERT INTO `users` (`id`, `username`, `password`, `email`, `logins`, `last_login`) VALUES
-(60, 'admin', 'fe9e44914dbb6bab93a29191b3c160f601a61e576a1da7693e', 'admin@admin.com', 39, 1238914359),
-(63, 'zeelot', '718cdc5d4e4372f50d4cb30ce217cb85fa117e734bb2d0a181', 'zeelot3k@gmail.com', 1, 1238913498);
+(60, 'admin', 'fc320cff004e31b7811d4770f6d3a6a77d10566fc54eedfd43', 'admin@admin.com', 39, 1238914359),
+(63, 'zeelot', '4a007c2707b09a0cce2cc81b392e917aff1b5affd9ca4025dd', 'zeelot3k@gmail.com', 11, 1241016426);
+
+DROP TABLE IF EXISTS `user_labels`;
+CREATE TABLE IF NOT EXISTS `user_labels` (
+  `id` int(11) NOT NULL auto_increment,
+  `user_id` int(11) unsigned NOT NULL,
+  `name` varchar(32) collate utf8_unicode_ci NOT NULL,
+  `value` varchar(127) collate utf8_unicode_ci NOT NULL,
+  PRIMARY KEY  (`id`,`user_id`),
+  KEY `user_labels_FKIndex1` (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+
+DROP TABLE IF EXISTS `user_settings`;
+CREATE TABLE IF NOT EXISTS `user_settings` (
+  `id` int(11) NOT NULL auto_increment,
+  `user_id` int(11) unsigned NOT NULL,
+  `label` varchar(32) collate utf8_unicode_ci NOT NULL,
+  `value` varchar(127) collate utf8_unicode_ci NOT NULL,
+  PRIMARY KEY  (`id`,`user_id`),
+  KEY `user_settings_FKIndex1` (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
 
 DROP TABLE IF EXISTS `user_tokens`;
 CREATE TABLE IF NOT EXISTS `user_tokens` (
@@ -181,15 +226,38 @@ CREATE TABLE IF NOT EXISTS `user_tokens` (
   KEY `user_tokens_FKIndex1` (`user_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
 
+ALTER TABLE `content_page_inheritances`
+  ADD CONSTRAINT `page_inheritances_ibfk_1` FOREIGN KEY (`content_page_id`) REFERENCES `content_pages` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION,
+  ADD CONSTRAINT `page_inheritances_ibfk_2` FOREIGN KEY (`inherited_page_id`) REFERENCES `content_pages` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION;
 
+--
+-- Constraints for table `content_pivots`
+--
 ALTER TABLE `content_pivots`
   ADD CONSTRAINT `has_nodes_ibfk_1` FOREIGN KEY (`content_node_id`) REFERENCES `content_nodes` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION,
   ADD CONSTRAINT `has_pages_ibfk_2` FOREIGN KEY (`content_page_id`) REFERENCES `content_pages` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION;
 
-
+--
+-- Constraints for table `roles_users`
+--
 ALTER TABLE `roles_users`
   ADD CONSTRAINT `roles_users_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION,
   ADD CONSTRAINT `roles_users_ibfk_2` FOREIGN KEY (`role_id`) REFERENCES `roles` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION;
 
+--
+-- Constraints for table `user_labels`
+--
+ALTER TABLE `user_labels`
+  ADD CONSTRAINT `user_labels_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION;
+
+--
+-- Constraints for table `user_settings`
+--
+ALTER TABLE `user_settings`
+  ADD CONSTRAINT `user_settings_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION;
+
+--
+-- Constraints for table `user_tokens`
+--
 ALTER TABLE `user_tokens`
   ADD CONSTRAINT `user_tokens_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION;
