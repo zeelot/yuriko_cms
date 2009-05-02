114625b1497169acb94328c0188049a44fdfd2c7
Lorenzo Pisani <zeelot3k@gmail.com>
2009-05-02 13:46:02 -0400

    simple admin start page for #10

--- modules/yuriko_core/controllers/admin/main.php
+++ modules/yuriko_core/controllers/admin/main.php
@@ -6,7 +6,7 @@ class Main_Controller extends Admin_Controller {
 
 	public function home()
 	{
-		
+		$this->template->content = View::factory('admin/content/main/home');
 	}
 
 } // End Admin Main Controller
