4930c5360703afa5a1af48dc315774fb358ac6e8
Lorenzo Pisani <zeelot3k@gmail.com>
2009-05-04 04:06:02 -0400

    added license to core models applies to #21

--- modules/yuriko_core/models/content_format.php
+++ modules/yuriko_core/models/content_format.php
@@ -1,5 +1,10 @@
-<?php
+<?php defined('SYSPATH') OR die('No direct access allowed.');
 
+/**
+* @package    YurikoCMS
+* @author     Lorenzo Pisani - Zeelot
+* @license    http://yurikocms.com/license
+*/
 
 class Content_Format_Model extends ORM{
 
--- modules/yuriko_core/models/content_page.php
+++ modules/yuriko_core/models/content_page.php
@@ -1,5 +1,11 @@
 <?php defined('SYSPATH') OR die('No direct access allowed.');
 
+/**
+* @package    YurikoCMS
+* @author     Lorenzo Pisani - Zeelot
+* @license    http://yurikocms.com/license
+*/
+
 class Content_Page_Model extends ORM {
 
 	protected $has_many = array
