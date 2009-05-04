1b54be7f9ed371f976a4327b42c6c61e9c4a89ac
Lorenzo Pisani <zeelot3k@gmail.com>
2009-05-04 05:31:54 -0400

    fixed little error

--- plugins/content_navigation/views/content/navigation/flat.php
+++ plugins/content_navigation/views/content/navigation/flat.php
@@ -9,7 +9,13 @@
 	<ul>
 	<?php foreach($node->subtree()->find_all() as $child): ?>
 		<li>
+		<?php if(($child->anchor) OR ($child->page_id > 0)): ?>
+		<?php echo ($child->page_id > 0)
+					? html::anchor(ORM::factory('content_page', $child->page_id)->alias, $child->name)
+					: html::anchor($child->anchor, $child->name) ?>
+		<?php else: ?>
 		<?php echo $child->name; ?>
+		<?php endif; ?>
 		</li>
 	<?php endforeach; ?>
 	</ul>
