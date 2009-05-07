d27e96400e9ee37711529e23e3d758db7f28c013
Lorenzo Pisani <zeelot3k@gmail.com>
2009-05-06 22:07:45 -0400

    fixing #33

--- themes/default/views/admin/plugins/info.php
+++ themes/default/views/admin/plugins/info.php
@@ -6,7 +6,13 @@
 		<li><strong>Version:</strong> <?php echo $plugin->version; ?></li>
 		<li><strong>Directory:</strong> <?php echo $plugin->dir; ?></li>
 		<?php foreach($plugin->dependencies as $dependency => $version): ?>
-		<li><strong>Required:</strong> <?php echo $dependency; ?> version <?php echo $version; ?></li>
+		<?php if (sizeof($version) == 1): ?>
+		<li><strong>Required:</strong>
+			<?php echo $dependency; ?> version <?php echo $version[0]; ?></li>
+		<?php elseif (sizeof($version) == 2): ?>
+		<li><strong>Required:</strong>
+			<?php echo $dependency; ?> versions <?php echo $version[0]; ?> to <?php echo $version[1]; ?></li>
+		<?php endif; ?>
 		<?php endforeach; ?>
 	</ul>
 </div>
