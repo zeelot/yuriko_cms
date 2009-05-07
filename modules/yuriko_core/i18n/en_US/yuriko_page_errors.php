0ac7f56847e37be128a59230e4328c7cc744323a
Lorenzo Pisani <zeelot3k@gmail.com>
2009-05-06 22:07:57 -0400

    fixed some validation problems

--- modules/yuriko_core/i18n/en_US/yuriko_page_errors.php
+++ modules/yuriko_core/i18n/en_US/yuriko_page_errors.php
@@ -25,9 +25,4 @@ $lang = array
 		'required' => 'You need to enter some content.',
 		'default'  => 'Invalid Input.',
 	),
-	'name' => array
-	(
-		'required' => 'You need to name the content.',
-		'default'  => 'Invalid Input.',
-	),
 );
--- modules/yuriko_core/libraries/MY_Validation.php
+++ modules/yuriko_core/libraries/MY_Validation.php
@@ -44,7 +44,6 @@ class Validation extends Validation_Core{
 		}
 		else
 		{
-
 			$errors = array();
 			foreach ($this->errors as $input => $error)
 			{
@@ -60,6 +59,215 @@ class Validation extends Validation_Core{
 
 			return $errors;
 		}
+	}
+
+	public function validate($object = NULL, $field_name = NULL)
+	{
+		if ($object === NULL)
+		{
+			// Use the current object
+			$object = $this;
+		}
+
+		// Get all field names
+		$fields = $this->field_names();
+
+		// Copy the array from the object, to optimize multiple sets
+		$array = $this->getArrayCopy();
+
+		foreach ($fields as $field)
+		{
+			if ($field === '*')
+			{
+				// Ignore wildcard
+				continue;
+			}
+
+			if ( ! isset($array[$field]))
+			{
+				if (isset($this->array_fields[$field]))
+				{
+					// This field must be an array
+					$array[$field] = array();
+				}
+				else
+				{
+					$array[$field] = NULL;
+				}
+			}
+		}
+
+		// Swap the array back into the object
+		$this->exchangeArray($array);
+
+		// Get all defined field names
+		$fields = array_keys($array);
+
+		foreach ($this->pre_filters as $field => $callbacks)
+		{
+			foreach ($callbacks as $callback)
+			{
+				if ($field === '*')
+				{
+					foreach ($fields as $f)
+					{
+						$this[$f] = is_array($this[$f]) ? array_map($callback, $this[$f]) : call_user_func($callback, $this[$f]);
+					}
+				}
+				else
+				{
+					$this[$field] = is_array($this[$field]) ? array_map($callback, $this[$field]) : call_user_func($callback, $this[$field]);
+				}
+			}
+		}
+
+		if ($this->submitted === FALSE)
+			return FALSE;
+
+		foreach ($this->rules as $field => $callbacks)
+		{
+			foreach ($callbacks as $callback)
+			{
+				// Separate the callback and arguments
+				list ($callback, $args) = $callback;
+
+				// Function or method name of the rule
+				$rule = is_array($callback) ? $callback[1] : $callback;
+
+				if ($field === '*')
+				{
+					foreach ($fields as $f)
+					{
+						// Note that continue, instead of break, is used when
+						// applying rules using a wildcard, so that all fields
+						// will be validated.
+
+						if (isset($this->errors[$f]))
+						{
+							// Prevent other rules from being evaluated if an error has occurred
+							continue;
+						}
+
+						if (empty($this[$f]) AND ! in_array($rule, $this->empty_rules))
+						{
+							// This rule does not need to be processed on empty fields
+							continue;
+						}
+
+						if ($args === NULL)
+						{
+							if ( ! call_user_func($callback, $this[$f]))
+							{
+								$this->errors[$f] = array($rule, NULL);
+
+								// Stop validating this field when an error is found
+								continue;
+							}
+						}
+						else
+						{
+							if ( ! call_user_func($callback, $this[$f], $args))
+							{
+								$this->errors[$f] = array($rule, $args);
+
+								// Stop validating this field when an error is found
+								continue;
+							}
+						}
+					}
+				}
+				else
+				{
+					if (isset($this->errors[$field]))
+					{
+						// Prevent other rules from being evaluated if an error has occurred
+						break;
+					}
+
+					if ( ! in_array($rule, $this->empty_rules) AND ! $this->required($this[$field]))
+					{
+						// This rule does not need to be processed on empty fields
+						continue;
+					}
+
+					if ($args === NULL)
+					{
+						if ( ! call_user_func($callback, $this[$field]))
+						{
+							$this->errors[$field] = array($rule, NULL);
+
+							// Stop validating this field when an error is found
+							break;
+						}
+					}
+					else
+					{
+						if ( ! call_user_func($callback, $this[$field], $args))
+						{
+							$this->errors[$field] = array($rule, NULL);
+
+							// Stop validating this field when an error is found
+							break;
+						}
+					}
+				}
+			}
+		}
+
+		foreach ($this->callbacks as $field => $callbacks)
+		{
+			foreach ($callbacks as $callback)
+			{
+				if ($field === '*')
+				{
+					foreach ($fields as $f)
+					{
+						// Note that continue, instead of break, is used when
+						// applying rules using a wildcard, so that all fields
+						// will be validated.
+
+						if (isset($this->errors[$f]))
+						{
+							// Stop validating this field when an error is found
+							continue;
+						}
+
+						call_user_func($callback, $this, $f);
+					}
+				}
+				else
+				{
+					if (isset($this->errors[$field]))
+					{
+						// Stop validating this field when an error is found
+						break;
+					}
+
+					call_user_func($callback, $this, $field);
+				}
+			}
+		}
+
+		foreach ($this->post_filters as $field => $callbacks)
+		{
+			foreach ($callbacks as $callback)
+			{
+				if ($field === '*')
+				{
+					foreach ($fields as $f)
+					{
+						$this[$f] = is_array($this[$f]) ? array_map($callback, $this[$f]) : call_user_func($callback, $this[$f]);
+					}
+				}
+				else
+				{
+					$this[$field] = is_array($this[$field]) ? array_map($callback, $this[$field]) : call_user_func($callback, $this[$field]);
+				}
+			}
+		}
+
+		// Return TRUE if there are no errors
+		return $this->errors === array();
 	}
 
 }
