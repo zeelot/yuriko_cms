3721ac73b24d668d5659fbab8d49216c906971d6
Lorenzo Pisani <zeelot3k@gmail.com>
2009-05-09 21:36:40 -0400

    jus making things prettier

--- plugins/content_feed/views/content/feed/default.php
+++ plugins/content_feed/views/content/feed/default.php
@@ -10,12 +10,19 @@
 ?>
 <div>
 	<h6 class="feed"><?php echo html::anchor($data->url, $data->title) ?></h6>
-	<ul>
-	<?php
-		foreach($data->items as $item):
-			$date = date('M j, g:i:s A', strtotime(isset($item['pubDate']) ? $item['pubDate'] : $item['updated']));
-	?>
-		<li><strong><?php echo html::specialchars($item['title']) ?></strong> &ndash; <?php echo $date ?> - <?php echo html::anchor(isset($item['link']) ? $item['link'] : $item['id'], 'Read More')?></li>
+	<dl>
+	<?php foreach($data->items as $item): ?>
+		<?php $date = date('M j, g:i:s A', strtotime(isset($item['pubDate'])
+				? $item['pubDate']
+				: $item['updated'])); ?>
+		<di><?php echo html::specialchars($item['title']) ?></di>
+		<dd>
+			<?php echo $date ?>
+			-
+			<?php echo html::anchor(isset($item['link'])
+				? $item['link']
+				: $item['id'], 'Read More')?>
+		</dd>
 	<?php endforeach;?>
-	</ul>
+	</dl>
 </div>
