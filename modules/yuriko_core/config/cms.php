0257c44cdeafa1ab48c041e5d54bee8fe7a3171b
Lorenzo Pisani <zeelot3k@gmail.com>
2009-05-13 21:17:38 -0400

    updated cms version definition

--- modules/yuriko_core/config/cms.php
+++ modules/yuriko_core/config/cms.php
@@ -1,5 +1,5 @@
 <?php defined('SYSPATH') OR die('No direct access allowed.');
 
-$config['version'] = '0.1.1';
-$config['codename'] = 'Caveman';
+$config['version'] = '0.2.0';
+$config['codename'] = 'Look! No Hands!';
 $config['status'] = 'alpha';
