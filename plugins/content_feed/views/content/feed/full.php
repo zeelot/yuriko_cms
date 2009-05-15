f97818a87dcd90c68510dc9284415651a9a46bff
Lorenzo Pisani <zeelot3k@gmail.com>
2009-05-15 13:24:58 -0400

    some feed view work

--- plugins/content_feed/views/content/feed/full.php
+++ plugins/content_feed/views/content/feed/full.php
@@ -1,5 +1,5 @@
 <?php
-/* 
+/*
  * This should get called by feed_content::render()
  *
  * Variables:
@@ -8,25 +8,24 @@
  */
 
 ?>
-<div>
-	<h6 class="feed">
+<div class="feed">
+	<h4>
 		<?php echo $data->title; ?>
 		<?php echo html::anchor($data->url, html::image(
 			'media/images/fam_silk/feed.png')) ?>
-	</h6>
-	<dl>
+	</h4>
+	<div>
 	<?php foreach($data->items as $item): ?>
 		<?php $date = date('M j, g:i:s A', strtotime(isset($item['pubDate'])
 				? $item['pubDate']
 				: $item['updated'])); ?>
-		<di><?php echo html::specialchars($item['title']) ?></di>
-		<dd>
-			<?php echo $date ?>
-			-
-			<?php echo html::anchor(isset($item['link'])
-				? $item['link']
-				: $item['id'], 'Read More')?>
-		</dd>
+		<h5>
+		<?php echo html::specialchars($item['title']) ?>
+		- <?php echo $date ?>
+		</h5>
+		<p>
+			<?php echo $item['content']; ?>
+		</p>
 	<?php endforeach;?>
-	</dl>
+	</div>
 </div>
