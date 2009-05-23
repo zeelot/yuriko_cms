f29aafe07772a7217ea5947aedac39e78f3658fa
Lorenzo Pisani <zeelot3k@gmail.com>
2009-05-22 20:31:31 -0400

    added install() and uninstall() to plugins controller

--- modules/yuriko_core/controllers/admin/plugins.php
+++ modules/yuriko_core/controllers/admin/plugins.php
@@ -30,11 +30,11 @@ class Plugins_Controller extends Admin_Controller {
 		if ((!$plugin->loaded) OR ($plugin->plugin_status->name == 'enabled'))
 			Event::run('system.404');
 		//if the plugin has an install script, run it.
-		//stop running when $_SESSION['yuriko.plugin_installed'] is TRUE
+		//stop running when $_SESSION['yuriko.plugin_setup'] is TRUE
 		if (
 			$plugin->plugin_status->name == 'uninstalled'
 			AND $plugin->installer == 1
-			AND (bool)!Session::instance()->get('yuriko.plugin_installed')
+			AND (bool)!Session::instance()->get('yuriko.plugin_setup')
 			)
 		{
 			//temporarily load the module to get to the installer files
@@ -66,12 +66,12 @@ class Plugins_Controller extends Admin_Controller {
 						notice::add($error, 'error');
 					}
 				}
-				Session::instance()->delete('yuriko.plugin_installed');
+				Session::instance()->delete('yuriko.plugin_setup');
 				url::redirect('admin/plugins/manage');
 			}
 			elseif(isset($_POST['cancel']))
 			{
-				Session::instance()->delete('yuriko.plugin_installed');
+				Session::instance()->delete('yuriko.plugin_setup');
 				notice::add('Action Cancelled!', 'success');
 				url::redirect('admin/plugins/manage');
 			}
@@ -84,6 +84,7 @@ class Plugins_Controller extends Admin_Controller {
 		$plugin = ORM::factory('plugin', $id);
 		if ((!$plugin->loaded) OR (!$plugin->plugin_status->name == 'enabled'))
 			Event::run('system.404');
+		//we aren't uninstalling the plugin, just disable it so it doesn't load
 		if (isset($_POST['confirm']))
 		{
 			//disable the plugin
@@ -111,6 +112,67 @@ class Plugins_Controller extends Admin_Controller {
 		}
 		$this->template->content = View::factory('admin/plugins/disable');
 		$this->template->content->plugin = $plugin;
+	}
+	public function install($id = NULL)
+	{
+		$plugin = ORM::factory('plugin', $id);
+		if ((!$plugin->loaded)) Event::run('system.404');
+		if (
+			$plugin->installer == 1
+			AND (bool)!Session::instance()->get('yuriko.plugin_setup')
+			)
+		{
+			//temporarily load the module to get to the installer files
+			Kohana::config_set('core.modules',
+				array_merge(Kohana::config('core.modules'),
+				 array(DOCROOT.'plugins/'.$plugin->dir)));
+			//run installation
+			$installation = Component::factory('admin/installer/'.$plugin->dir)
+				->method('install', array($plugin));
+			$this->template->content = $installation;
+		}
+		else
+		{
+			//installation complete, redirect
+			notice::add('Plugin Installed', 'success');
+			url::redirect('admin/plugins/manage');
+		}
+	}
+	public function uninstall($id = NULL)
+	{
+		$plugin = ORM::factory('plugin', $id);
+		if ((!$plugin->loaded)
+			OR ($plugin->plugin_status->name == 'uninstalled')
+			OR ($plugin->installer == 0)
+			)
+			Event::run('system.404');
+		if ((bool)!Session::instance()->get('yuriko.plugin_setup'))
+		{
+			//run uninstallation
+			$installation = Component::factory('admin/installer/'.$plugin->dir)
+				->method('uninstall', array($plugin));
+			$this->template->content = $installation;
+		}
+		else
+		{
+			//change plugin status
+			$array = $plugin->as_array();
+			$array['action'] = 'uninstall';
+			$array['plugin_status_id'] = ORM::factory('plugin_status', 'uninstalled')->id;
+			if ($plugin->validate($array))
+			{
+				$plugin->save();
+				notice::add('Plugin Uninstalled', 'success');
+			}
+			else
+			{
+				foreach($array->errors('plugin_errors') as $error)
+				{
+					notice::add($error, 'error');
+				}
+			}
+			url::redirect('admin/plugins/manage');
+		}
 	}
 
 } // End Admin Plugins Controller
--- plugins/content_basic/controllers/admin/installer/content/basic.php
+++ plugins/content_basic/controllers/admin/installer/content/basic.php
@@ -16,11 +16,63 @@ class Admin_Installer_Content_Basic_Controller extends Controller {
 
 	public function install(Plugin_Model $plugin)
 	{
-		echo 'DO INSTALL! '.$plugin->name;
-		Session::instance()->set('yuriko.plugin_installed', TRUE);
+		// Taken from Argentum Invoice http://argentuminvoice.com - THANKS ZOMBOR!
+		$tables = View::factory('admin/installer/content/basic/schema')->render();
+		$db = Database::instance();
+		$query = '';
+		foreach (explode("\n", $tables) as $sql)
+		{
+			if (trim($sql) != "" AND strpos($sql, "--") === FALSE)
+			{
+				$query .= $sql;
+				if (preg_match("/;[\040]*\$/", $sql))
+				{
+					$db->query($query);
+					$query = '';
+				}
+			}
+		}
+		notice::add('DB Table for Basic Content Plugin Created', 'success');
+		Session::instance()->set('yuriko.plugin_setup', TRUE);
+		url::redirect(Router::$current_uri);
 	}
 	public function uninstall()
 	{
-		echo 'DO UNINSTALL!';
+		if (isset($_POST['confirm']))
+		{
+			//uninstall plugin
+			// Taken from Argentum Invoice http://argentuminvoice.com - THANKS ZOMBOR!
+			$tables = View::factory('admin/installer/content/basic/uninstall')->render();
+			$db = Database::instance();
+			$query = '';
+			foreach (explode("\n", $tables) as $sql)
+			{
+				if (trim($sql) != "" AND strpos($sql, "--") === FALSE)
+				{
+					$query .= $sql;
+					if (preg_match("/;[\040]*\$/", $sql))
+					{
+						$db->query($query);
+						$query = '';
+					}
+				}
+			}
+			notice::add('DB Table for Basic Content Plugin Dropped', 'success');
+			//set session var to tell main setup that plugin is done uninstalling
+			Session::instance()->set('yuriko.plugin_setup', TRUE);
+			//redirect to current url to finish uninstallation
+			url::redirect(Router::$current_uri);
+		}
+		elseif(isset($_POST['cancel']))
+		{
+			notice::add('Action Cancelled!', 'success');
+			//no need to finish uninstallation
+			url::redirect('admin/plugins/manage');
+		}
+		else
+		{
+			echo View::factory('forms/simple_confirmation')
+				->set('message', 'This will delete the Basic_Content table and all its contents.');
+		}
 	}
 }
