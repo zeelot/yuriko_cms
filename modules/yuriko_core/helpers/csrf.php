55c1cca93a4768865f975e6afa7fa280f025d9d2
Lorenzo Pisani <zeelot3k@gmail.com>
2009-05-23 18:41:56 -0400

    OCD FTW! fixed tabs in csrf helper

--- modules/yuriko_core/helpers/csrf.php
+++ modules/yuriko_core/helpers/csrf.php
@@ -5,16 +5,15 @@
 
 class csrf_Core {
 
-  public static function token()
-  {
-    if (($token = Session::instance()->get('csrf')) === FALSE)
-		Session::instance()->set('csrf', ($token = text::random('alnum', 16)));
-    return $token;
-  }
+	public static function token()
+	{
+		if (($token = Session::instance()->get('csrf')) === FALSE)
+			Session::instance()->set('csrf', ($token = text::random('alnum', 16)));
+		return $token;
+	}
 
-  public static function valid($token)
-  {
-    return ($token === arr::remove('csrf', $_SESSION));
-  }
-
+	public static function valid($token)
+	{
+		return ($token === arr::remove('csrf', $_SESSION));
+	}
 }
