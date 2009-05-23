0a34d1890ffd7f933f51884552b1608c99545619
Lorenzo Pisani <zeelot3k@gmail.com>
2009-05-22 20:30:46 -0400

    added a notice section for simple_confirmation forms

--- themes/default/views/forms/simple_confirmation.php
+++ themes/default/views/forms/simple_confirmation.php
@@ -2,6 +2,9 @@
 	<?php echo form::open(); ?>
 	<fieldset>
 		<legend>Continue with Action?</legend>
+		<?php if (isset($message)): ?>
+		<p class="notice"><?php echo $message; ?></p>
+		<?php endif; ?>
 		<button type="submit" name="confirm" value="confirm">Confirm</button>
 		<button type="submit" name="cancel" value="cancel">Cancel</button>
 	</fieldset>
